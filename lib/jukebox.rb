28a47b696ff53f2aa0bfa0b57cc522f87bbf8c88
Valentin Placido <valentinplacido25@gmail.com>
2020-01-25 04:13:55 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,16 +31,24 @@ def exit_jukebox
   puts "Goodbye"
 end 
 
+def prompt
+  puts "Please enter a command:"
+  gets.strip
+end
+
 def run(songs)
   puts "Please enter a command:"
   input = gets.strip
   while input != "exit" do 
     if input == "help"
       help 
+      input = prompt
     elsif input == "list"
       list(songs)
+      input = prompt
     elsif input == "play"
       play(songs)
+      input = prompt
     end
   end
   exit_jukebox
