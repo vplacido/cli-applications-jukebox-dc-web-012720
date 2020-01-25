85c86afa5bf53d32505e9029af0fc3388dbb9c7e
Valentin Placido <valentinplacido25@gmail.com>
2020-01-25 03:50:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,4 +1,15 @@
 # Add your code here
 def help 
-  puts 
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end 
+
+def list(songs)
+  i = 1
+  songs.each do |song|
+    puts "#{i}. #{song}"
+  end 
+end
